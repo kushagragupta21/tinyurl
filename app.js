fb25c2bd2bce5a093b0f14844f40cb2255179ae1
Kushagra <kushagragupta6038@gmail.com>
2021-05-17 16:41:38 +0530

    tiny-url

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
 const app = express();
 
 
+mongoose.connect('mongodb://localhost/urlShortner',{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+})
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', async(req, res) =>{
+    const shortUrls =await ShortUrl.find();
+    res.render('index',{shortUrls: shortUrls})
+})
+app.post('/postUrls', async (req, res) =>{
+   await shortUrl.create({ full: req.body.fullURL});
+   res.redirect('/')
+})
+
+app.get('/:shortUrl',async (req, res) =>{
+   const shorturl = await ShortUrl.findOne({short: req.params.shortUrl })
+
+   if(shorturl == null) return res.sendStatus(404);
+
+   shorturl.clicks++
+   shorturl.save() // To update clicks in databse
+
+   res.redirect(shorturl.full);
+})
 
 app.listen(process.env.PORT || 5000);
